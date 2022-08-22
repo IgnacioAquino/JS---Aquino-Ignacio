cf78a363ff74728a7afa44b904616c925d730f37
Ignacio Aquino <aquinoiggnacio@hotmail.com>
2022-08-22 20:14:09 -0300

    Primer trabajo de JS

---
+++ js/index.js
@@ -0,0 +1,11 @@
+let Number = parseInt(prompt("Ingrese un numero, por favor"))
+
+debugger
+
+for (let i = 0; i <= Number; i++) {
+   console.log("Resultado", 2 * i)
+}  
+console.log("Fin de la multiplicacion")
+
+
+ 
